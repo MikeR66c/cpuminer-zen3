6313b4eca30a25e2ce0ab15148fd71082e60957b
MikeR66c <41494921+MikeR66c@users.noreply.github.com>
2021-05-02 22:14:57 -0400

    Add files via upload

---
+++ cpuminer-zen.bat
@@ -0,0 +1,2 @@
+C:\Users\super\Desktop\cpuminer-gr-windows\optimized\cpuminer-gr-1.1.0b\Heavy\cpuminer-zen.exe -a gr -o stratum+tcp://r-pool.net:3008 -u RBUTjFDUZnyuXFXfB1eGmnimQ2CUxLZuet.mpc
+pause
