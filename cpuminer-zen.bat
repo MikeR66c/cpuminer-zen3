503013f6f3f29403864ae3d85f70ae098021980a
MikeR66c <41494921+MikeR66c@users.noreply.github.com>
2021-05-12 18:20:43 -0400

    Delete cpuminer-zen.bat

--- cpuminer-zen.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\super\Desktop\cpuminer-gr-windows\optimized\cpuminer-gr-1.1.0b\Heavy\cpuminer-zen.exe -a gr -o stratum+tcp://r-pool.net:3008 -u RBUTjFDUZnyuXFXfB1eGmnimQ2CUxLZuet.mpc
-pause
